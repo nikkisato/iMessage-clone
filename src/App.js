df57fa4429081a2ec200ab96f6b9038149cfac5a
Nikki <nikki.satopdx@gmail.com>
2021-03-17 21:24:49 -0700

    able to create a name

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Imessage from './Component/Imessage/Imessage';
+import { useSelector, useDispatch } from 'react-redux';
+import { selectUser, login, logout } from './features/userSlice';
+import Login from './Component/Login/Login';
+import { auth } from './firebase';
 
 function App() {
-  return (
-    <div className='app'>
-      <Imessage />
-    </div>
-  );
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged(authUser => {
+      if (authUser) {
+        //user is logged in
+        dispatch(
+          login({
+            uid: authUser.uid,
+            photo: authUser.photoUrl,
+            email: authUser.email,
+            displayName: authUser.displayName,
+          })
+        );
+      } else {
+        // user is logged out
+        dispatch(logout());
+      }
+    });
+  }, []);
+
+  return <div className='app'>{user ? <Imessage /> : <Login />}</div>;
 }
 
 export default App;
--- src/Component/Chat/Chat.js
+++ src/Component/Chat/Chat.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Chat.css';
+import MicNoneIcon from '@material-ui/icons/MicNone';
+
+import { IconButton } from '@material-ui/core';
+
 function Chat() {
+  const [input, setInput] = useState('');
+  const sendMessage = e => {
+    e.preventDefault();
+    console.log('clicked');
+
+    setInput('');
+  };
+
   return (
     <div className='chat'>
       <div className='chat__header'>
@@ -9,8 +21,25 @@ function Chat() {
         </h4>
         <strong>Details</strong>
       </div>
-      <div className='chat__body'></div>
-      <div className='chat__input'></div>
+      <div className='chat__messages'>
+        <h2>I am a message</h2>
+      </div>
+      <div className='chat__input'>
+        <form>
+          <input
+            value={input}
+            placeholder='iMessage'
+            type='text'
+            onChange={e => {
+              setInput(e.target.value);
+            }}
+          />
+          <button onClick={sendMessage}>Send Message</button>
+        </form>
+        <IconButton>
+          <MicNoneIcon className='chat__mic' />
+        </IconButton>
+      </div>
     </div>
   );
 }
